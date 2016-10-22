107b2831f2f34f4222395a9ed829a908641b86de
Mathias <Mathias@LAPTOP-84H7VQ2I>
2016-10-22 17:29:14 +0200

    LELELELELELL

---
+++ TheLegendOfAsger/Arena.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using GameLogic;
+
+namespace TheLegendOfAsger
+{
+    class Arena
+    {
+        ICreature[] enemyList = new ICreature[]
+        {
+            new Enemy("Street dog", 5, 5, 1),
+            new Enemy("Local drunk", 6, 6, 1),
+            new Enemy("Weak, sick, handicapped and wounded Goblin", 7, 7, 2)
+        };
+
+        ICreature enemy;
+
+        public void ArenaLoop(Player player)
+        {
+            bool running = true;
+            string input;
+
+            do
+            {
+                Console.Clear();
+                DisplayStats(player);
+                DisplayChoices();
+
+                input = Console.ReadLine();
+
+                switch (input.ToLower())
+                {
+                    case "1":
+                        enemy = enemyList[0];
+                        BattleArena(player, enemy);
+                        break;
+                    case "2":
+                        enemy = enemyList[1];
+                        BattleArena(player, enemy);
+                        break;
+                    case "3":
+                        enemy = enemyList[2];
+                        BattleArena(player, enemy);
+                        break;
+                    case "e":
+                        running = false;
+                        break;
+                    default:
+                        Game.DisplayInputError();
+                        break;
+                }
+            }
+            while (running);
+        }
+
+        public void DisplayChoices()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Choose an opponent or E to exit:");
+
+            int i = 1;
+            foreach (ICreature enemy in enemyList)
+            {
+                Console.WriteLine(" {0}. {1}", i, enemy.Name);
+                i++;
+            }
+        }
+
+        public void BattleArena(Player player, ICreature enemy)
+        {
+            GameController gameController = new GameController();
+
+            Console.Clear();
+            DisplayStats(player);
+            Console.WriteLine(" VS");
+            DisplayStats(enemy);
+            Console.WriteLine();
+            Console.Write("Press enter to start the battle..");
+            Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine("{0} did {1} damage to {2}", player.Name, player.Attack, enemy.Name);
+            Console.WriteLine("{0} counterattacked for {1} damage!", enemy.Name, enemy.Attack);
+            Console.WriteLine();
+
+            BattleOutcome outcome = gameController.CheckForWinner(player, enemy);
+
+            if (outcome == BattleOutcome.Win)
+                Console.Write("{0} has beaten {1}!", player.Name, enemy.Name);
+            else if (outcome == BattleOutcome.Draw)
+                Console.Write("{0} and {1} both died in the tragic battle.", player.Name, enemy.Name);
+            else
+                Console.Write("{0} was defeated by {1}!", player.Name, enemy.Name);
+
+            Console.ReadLine();
+        }
+
+        public void DisplayStats(ICreature creature)
+        {
+            Console.WriteLine("Name: {0}", creature.Name);
+            Console.WriteLine("HP: {0}", creature.Hp);
+            Console.WriteLine("Attack: {0}", creature.Attack);
+        }
+    }
+}
--- TheLegendOfAsger/Game.cs
+++ TheLegendOfAsger/Game.cs
@@ -1,23 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 using GameLogic;
 
 namespace TheLegendOfAsger
 {
     class Game
     {
+        
+
         public void Run()
         {
             bool running = true;
             string input;
             Player player = new Player();
-            ICreature enemy;
 
             DisplayTitleScreen();
 
             do
             {
                 Console.Clear();
-                DisplayStats(player);
                 DisplayChoices();
 
                 input = Console.ReadLine();
@@ -25,16 +26,12 @@ namespace TheLegendOfAsger
                 switch (input)
                 {
                     case "1":
-                        enemy = new Enemy("Street dog", 5, 5);
-                        BattleArena(player, enemy);
+                        Arena arena = new Arena();
+                        arena.ArenaLoop(player);
                         break;
                     case "2":
-                        enemy = new Enemy("Local drunk", 6, 6);
-                        BattleArena(player, enemy);
                         break;
                     case "3":
-                        enemy = new Enemy("Weak, sick, handicapped and wounded Goblin", 7, 7);
-                        BattleArena(player, enemy);
                         break;
                     default:
                         DisplayInputError();
@@ -66,55 +63,17 @@ namespace TheLegendOfAsger
             Console.ReadLine();
         }
 
-        public void DisplayStats(ICreature creature)
-        {
-            Console.WriteLine("Name: {0}", creature.Name);
-            Console.WriteLine("HP: {0}", creature.Hp);
-            Console.WriteLine("Attack: {0}", creature.Attack);
-        }
-
         public void DisplayChoices()
         {
-            Console.WriteLine();
-            Console.WriteLine("Choose an opponent:");
-            Console.WriteLine(" 1. Street dog");
-            Console.WriteLine(" 2. Local drunk");
-            Console.WriteLine(" 3. Weak, sick, handicapped and wounded Goblin");
+            Console.WriteLine("Choose an option: ");
+            Console.WriteLine(" 1. Arena");
         }
 
-        public void DisplayInputError()
+        public static void DisplayInputError()
         {
             Console.WriteLine("You've got fat fingers! Now try again..");
             Console.ReadLine();
         }
-
-        public void BattleArena(ICreature creatureOne, ICreature creatureTwo)
-        {
-            GameController gameController = new GameController();
-
-            Console.Clear();
-            DisplayStats(creatureOne);
-            Console.WriteLine(" VS");
-            DisplayStats(creatureTwo);
-            Console.WriteLine();
-            Console.Write("Press enter to start the battle..");
-            Console.ReadLine();
-
-            Console.WriteLine();
-            Console.WriteLine("{0} did {1} damage to {2}", creatureOne.Name, creatureOne.Attack, creatureTwo.Name);
-            Console.WriteLine("{0} counterattacked for {1} damage!", creatureTwo.Name, creatureTwo.Attack);
-            Console.WriteLine();
-
-            BattleOutcome outcome = gameController.CheckForWinner(creatureOne, creatureTwo);
-
-            if (outcome == BattleOutcome.Win)
-                Console.Write("{0} has beaten {1}!", creatureOne.Name, creatureTwo.Name);
-            else if (outcome == BattleOutcome.Draw)
-                Console.Write("{0} and {1} both died in the tragic battle.", creatureOne.Name, creatureTwo.Name);
-            else
-                Console.Write("{0} was defeated by {1}!", creatureOne.Name, creatureTwo.Name);
-
-            Console.ReadLine();
-        }
+        
     }
 }
