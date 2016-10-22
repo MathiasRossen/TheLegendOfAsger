6f00d738bdffc496e9ad91b62cd4a34d7d530143
Mathias <Mathias@LAPTOP-84H7VQ2I>
2016-10-22 15:15:46 +0200

    Added title screen

---
+++ TheLegendOfAsger/Game.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TheLegendOfAsger
+{
+    class Game
+    {
+        public void Run()
+        {
+            bool running = true;
+            string input;
+
+            DisplayTitleScreen();
+
+            do
+            {
+                input = Console.ReadLine();
+            }
+            while (running);
+        }
+
+        public void DisplayTitleScreen()
+        {
+            Console.Write("Welcome to...");
+            Console.ReadLine();
+
+            Console.WriteLine("########################################################");
+            Console.WriteLine("# ▄▄▄ ▄  ▄ ▄▄▄   ▄   ▄▄▄ ▄▄▄▄ ▄▄▄ ▄  ▄ ▄▄▄     ▄▄  ▄▄▄ #");
+            Console.WriteLine("#  █  █▄▄█ █▄    █   █▄  █ ▄▄ █▄  █▄ █ █  █   █  █ █▄  #");
+            Console.WriteLine("#  █  █  █ █▄▄   █▄▄ █▄▄ █▄▄█ █▄▄ █ ▀█ █▄▄▀   ▀▄▄▀ █   #");
+            Console.WriteLine("#                                                      #");
+            Console.WriteLine("#                                                      #");
+            Console.WriteLine("#      ██       ██████    ██████    ██████   █████     #");
+            Console.WriteLine("#     ████     ██        ██         ██       ██   ██   #");
+            Console.WriteLine("#    ██  ██      ███     ██  ███    ████     ██   ██   #");
+            Console.WriteLine("#   ████████        ██   ██    ██   ██       █████     #");
+            Console.WriteLine("#   ██    ██   ██████     ██████    ██████   ██   ██   #");
+            Console.WriteLine("#                                                      #");
+            Console.WriteLine("########################################################");
+
+            Console.ReadLine();
+        }
+    }
+}
